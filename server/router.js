d94e8c48a5d693615b9ce1507f003cc9a5042890
Leesanghun0226 <fkdndpf1@gmail.com>
2017-09-13 04:34:19 +0900

    sire router

--- server/router.js
+++ server/router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const ProductController = require('./controllers/product');
+const SireController = require('./controllers/sire');
 
 module.exports = function (app) {
   const apiRoutes = express.Router();
   const productRoutes = express.Router();
+  const sireRoutes = express.Router();
   /*------------------------------------------------------------------------------
     PRODUCT ROUTE
   ------------------------------------------------------------------------------*/
@@ -14,6 +16,11 @@ module.exports = function (app) {
   productRoutes.get('/:productname', ProductController.oneProductGet);
   productRoutes.post('/', ProductController.saveProduct);
 
+  apiRoutes.use('/sire', sireRoutes);
+
+  sireRoutes.get('/', SireController.allSireGet);
+  sireRoutes.get('/:productname', SireController.oneSireGet);
+  sireRoutes.post('/', SireController.saveSire);
 
   app.use('/api', apiRoutes);
 };
